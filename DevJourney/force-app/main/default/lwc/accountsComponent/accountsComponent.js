3d0610828f8a0c2abaee75759f18f3cb84e41c64
AmigoBongero <kirill14062002@gmail.com>
2025-01-30 12:51:25 +0300

    # Wrote MyAccounts tab
    - dev: MyAccounts tab functuinality
    - dev: myAccounts Apex Controller
    - fix: gitignore

--- DevJourney/force-app/main/default/lwc/accountsComponent/accountsComponent.js
+++ DevJourney/force-app/main/default/lwc/accountsComponent/accountsComponent.js
@@ -1,4 +1,18 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import getMyAccounts from "@salesforce/apex/myAccountsController.getMyAccounts"
 export default class AccountsComponent extends LightningElement {
+    columns = [
+        {label: 'Name', fieldName: 'Name'},
+        {label: 'Industry', fieldName: 'Industry'},
+        {label: 'Phone', fieldName: 'Phone'}
+    ];
+    data = [];
+    @wire(getMyAccounts)
+    accounts ({data,error}){
+        if (data) {
+            this.data = data;
+        } else if (error) {
+            console.error(error);
+        }
+    }
 }
