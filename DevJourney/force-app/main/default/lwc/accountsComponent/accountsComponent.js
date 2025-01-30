2269d976dab69ae7b9c27a32171176f9b5652998
AmigoBongero <kirill14062002@gmail.com>
2025-01-30 14:47:31 +0300

    # Developed recently viewed accounts tab
    - Dev: recently viewed accounts tab
    - Dev: apex method for recently viewed accounts tab

--- DevJourney/force-app/main/default/lwc/accountsComponent/accountsComponent.js
+++ DevJourney/force-app/main/default/lwc/accountsComponent/accountsComponent.js
@@ -1,18 +1,37 @@
-import { LightningElement, wire } from 'lwc';
-import getMyAccounts from "@salesforce/apex/myAccountsController.getMyAccounts"
+import { LightningElement} from 'lwc';
+import getMyAccounts from "@salesforce/apex/AccountController.getMyAccounts";
+import getRecentlyViewedAccounts from "@salesforce/apex/AccountController.getRecentlyViewedAccounts";
 export default class AccountsComponent extends LightningElement {
     columns = [
         {label: 'Name', fieldName: 'Name'},
         {label: 'Industry', fieldName: 'Industry'},
         {label: 'Phone', fieldName: 'Phone'}
     ];
-    data = [];
-    @wire(getMyAccounts)
-    accounts ({data,error}){
-        if (data) {
-            this.data = data;
-        } else if (error) {
-            console.error(error);
-        }
+    myAccountsData = [];
+    recentlyViewedAccountsData = [];
+
+    connectedCallback() {
+        this.fetchMyAccounts();
+        this.fetchRecentlyViewedAccounts();
+    }
+
+    fetchMyAccounts() {
+        getMyAccounts()
+            .then(result => {
+                this.myAccountsData = result;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    fetchRecentlyViewedAccounts() {
+        getRecentlyViewedAccounts()
+            .then(result => {
+                this.recentlyViewedAccountsData = result;
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 }
