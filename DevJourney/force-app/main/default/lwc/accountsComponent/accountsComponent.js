bc9ab437e65316e30e7cf429d7cfaf1b999ec404
AmigoBongero <kirill14062002@gmail.com>
2025-02-05 11:01:52 +0300

    #Correctrions

--- DevJourney/force-app/main/default/lwc/accountsComponent/accountsComponent.js
+++ DevJourney/force-app/main/default/lwc/accountsComponent/accountsComponent.js
@@ -1,13 +1,13 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-import getMyAccounts from "@salesforce/apex/AccountsComponentController.getMyAccounts";
-import getRecentlyViewedAccounts from "@salesforce/apex/AccountsComponentController.getRecentlyViewedAccounts";
+import getMyAccounts from '@salesforce/apex/AccountsComponentController.getMyAccounts';
+import getRecentlyViewedAccounts from '@salesforce/apex/AccountsComponentController.getRecentlyViewedAccounts';
 
 const ACCOUNT_COLUMNS = [
-    {label: 'Name', fieldName: 'Name'},
-    {label: 'Industry', fieldName: 'Industry'},
-    {label: 'Phone', fieldName: 'Phone'}
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Industry', fieldName: 'Industry' },
+    { label: 'Phone', fieldName: 'Phone' }
 ];
 
 export default class AccountsComponent extends LightningElement {
