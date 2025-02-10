d90897acbb872e21afa4050215e0eb409a1e36ee
Asha2502 <lisnevskaya08@gmail.com>
2025-02-10 15:12:21 +0300

    #5 Fixed style

--- DevJourney/force-app/main/default/lwc/allAccountsTab/allAccountsTab.js
+++ DevJourney/force-app/main/default/lwc/allAccountsTab/allAccountsTab.js
@@ -1,5 +1,7 @@
 import { LightningElement, track, wire } from 'lwc';
+
 import getAccountList from "@salesforce/apex/AccountComponentController.getAccountList";
+
 import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
 import ACCOUNT_TYPE_FIELD from '@salesforce/schema/Account.Type';
 import ACCOUNT_PHONE_FIELD from '@salesforce/schema/Account.Phone';
@@ -7,14 +9,49 @@ import ACCOUNT_WEBSITE_FIELD from '@salesforce/schema/Account.Website';
 import CONTACT_NAME_FIELD from '@salesforce/schema/Contact.Name';
 import CONTACT_PHONE_FIELD from '@salesforce/schema/Contact.Phone';
 import CONTACT_EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+
 export default class AllAccountsTab extends LightningElement {
+    //Tree data variables
     @track treeData = [];
     error = null;
     isVisible = false;
+
+    //Record-view-form fields	
     records = null;
     fieldsAccount = [ACCOUNT_NAME_FIELD, ACCOUNT_TYPE_FIELD, ACCOUNT_PHONE_FIELD, ACCOUNT_WEBSITE_FIELD];
     fieldsContact = [CONTACT_NAME_FIELD, CONTACT_PHONE_FIELD, CONTACT_EMAIL_FIELD];
+    
+    /*
+    * @description Wire function. 
+    */
+    @wire(getAccountList)
+    wiredAccounts({ error, data }) {
+        if (data && Array.isArray(data)) {
+            this.treeData = data.map(account => ({
+                label: account.Name,
+                name: account,
+                accountData: account,
+                items: (account.Contacts || []).map(contact => ({
+                    label: contact.Name,
+                    name: contact,
+                    contactData: contact,
+                    parentAccount: account
+                }))
+            }));
+            this.error = undefined;
+        } else if (error) {
+            this.treeData = [];
+            this.error = error.body.message;
+        }
+        else {
+            this.treeData = [];
+            this.error = 'No data received';
+        }
+    }
 
+    /*
+    * @description Handler.
+    */
     handleSelect(event) {
         this.isVisible = true;
 
@@ -42,31 +79,6 @@ export default class AllAccountsTab extends LightningElement {
                     objectApiName: 'Account',
                     title: 'Account Info'
                 }];
-        }
-    }
-
-    @wire(getAccountList)
-    wiredAccounts({ error, data }) {
-        if (data && Array.isArray(data)) {
-            this.treeData = data.map(account => ({
-                label: account.Name,
-                name: account,
-                accountData: account,
-                items: (account.Contacts || []).map(contact => ({
-                    label: contact.Name,
-                    name: contact,
-                    contactData: contact,
-                    parentAccount: account
-                }))
-            }));
-            this.error = undefined;
-        } else if (error) {
-            this.treeData = [];
-            this.error = error.body.message;
-        }
-        else {
-            this.treeData = [];
-            this.error = 'No data received';
         }
     }
 
--- DevJourney/force-app/main/default/lwc/summaryInfo/summaryInfo.js
+++ DevJourney/force-app/main/default/lwc/summaryInfo/summaryInfo.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
 export default class SummaryInfo extends LightningElement {
+    //API variable
     @api records;
 }
