23388bff7685202d470bf7b9acb55411ad5371b2
Kirill <118204572+AmigoBongero@users.noreply.github.com>
2025-03-04 17:20:41 +0300

    Add counter reset in expensesTabComponent

--- DevJourney/force-app/main/default/lwc/expensesTabComponent/expensesTabComponent.js
+++ DevJourney/force-app/main/default/lwc/expensesTabComponent/expensesTabComponent.js
@@ -145,6 +145,7 @@ export default class ExpensesTabComponent extends LightningElement {
         getExpenses()
             .then(result => {
                 this.expensesFullData = result;
+                this.expensesRecordCount = 20;
                 this.expensesData = this.expensesFullData.slice(0, this.expensesRecordCount);
                 this.selectedExpenseIds = [];
             }).catch(error => {
